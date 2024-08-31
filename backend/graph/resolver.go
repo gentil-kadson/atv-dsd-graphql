7df11f5b35d2ebe074dfd2bc59a92791b2c6f79b
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2024-08-31 13:35:51 -0300

    feature(backend): graphQL base backend working

---
+++ backend/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/gentil-kadson/atv-dsd-graphql/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct{
+	projects []*model.Project
+}
---
+++ backend/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gentil-kadson/atv-dsd-graphql/graph"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"},
+		AllowCredentials: true,
+		Debug: true,
+	}).Handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv.AddTransport((&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return r.Host == "http://localhost:5173"
+			},
+			ReadBufferSize: 1024,
+			WriteBufferSize: 1024,
+		},
+	}))
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://192.168.0.104:%s/ for GraphQL playground", port)
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		panic(err)
+	}
+}
