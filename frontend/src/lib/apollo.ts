85c6f3d0c3e10f3ca8837cfe6d5dc727fcc92f56
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-08-31 15:30:59 -0300

    renames api url to localhost.

--- frontend/src/lib/apollo.ts
+++ frontend/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://192.168.0.104:8080/query", // Atualize com a URL da sua API GraphQL
+  uri: "http://localhost:8080/query", // Atualize com a URL da sua API GraphQL
   cache: new InMemoryCache(),
 });
